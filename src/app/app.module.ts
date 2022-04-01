11ab61ba90d201880cf3f40b6580b3925f712714
apgallego <apgallego@users.noreply.github.com>
2022-04-01 14:39:54 +0200

    template and reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
 import { FormNewItemComponent } from './form-new-item/form-new-item.component';
 import { CitiesComponent } from './cities/cities.component';
 import { FilterPipe } from './pipes/filter.pipe';
+import { ContactComponent } from './contact/contact.component';
+import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,14 @@ import { FilterPipe } from './pipes/filter.pipe';
     FormNewItemComponent,
     CitiesComponent,
     FilterPipe,
+    ContactComponent,
+    ContactReactiveComponent,
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-reactive/contact-reactive.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-reactive',
+  templateUrl: './contact-reactive.component.html',
+  styleUrls: ['./contact-reactive.component.scss']
+})
+export class ContactReactiveComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.initForm();
+    this.onPatchValue();
+    // this.onSetValue();
+  }
+
+  onPatchValue(): void{
+    //this makes possible to store data on the properties we write.
+    this.contactForm.patchValue({ name: 'Alfre' })
+  }
+  
+  onSetValue(): void{
+    //this works in the same way but requires all the different properties of the object.
+    //(if executed like this, an error will be shown in the console)
+    this.contactForm.setValue({ comment: 'Hola mundo' })
+  }
+
+  onSubmit(): void{
+    console.log('Form -> ');
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      checkAdult: ['', [Validators.required]],
+      department: [''],
+      comment: ['', [Validators.required]],
+    })
+  }
+}
