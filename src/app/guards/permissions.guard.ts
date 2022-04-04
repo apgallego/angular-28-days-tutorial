50e1755fc9d5e8ae8c9c03a8595970a14b9fcec3
apgallego <apgallego@users.noreply.github.com>
2022-04-05 00:14:57 +0200

    guards

---
+++ src/app/guards/permissions.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionsGuard implements CanActivate {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(this.hasUser()){
+      return true;
+    } else {
+      //Redirect login
+      alert('You don\'t have permissions');
+      return false;
+    }
+  }
+
+  hasUser(): boolean {
+    return false;
+  }
+  
+}
